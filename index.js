14e2a065eaef58d25b1b63bcfe28752932b1ff45
Daniel Mueller <danielpetermueller@gmail.com>
2021-05-17 19:38:37 -0400

    commit tree 651201f6cf26410c37b0b54667d182798d707acd

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const headQ = 42
+function distanceFromHqInBlocks(pickup){
+    if(pickup > 42){
+        return (pickup - headQ);
+    } else {
+        return (headQ - pickup);
+    }
+}
+function distanceFromHqInFeet(pickup){
+       return distanceFromHqInBlocks(pickup)*264;
+}
+function distanceTravelledInFeet(a,b){
+    if(a<b){
+        return (b-a)*264;
+    }else{
+        return (a-b)*264;
+    }
+}
+function calculatesFarePrice(a,b){
+    if (distanceTravelledInFeet(a,b) <= 400){
+    return 0;
+}
+else if (distanceTravelledInFeet(a,b) <= 2000){
+    return (distanceTravelledInFeet(a,b) - 400) * .02;
+}
+else if (distanceTravelledInFeet(a,b) < 2500){
+    return 25;
+}
+else if (distanceTravelledInFeet(a,b) > 2500){
+    return 'cannot travel that far';
+}
+}
